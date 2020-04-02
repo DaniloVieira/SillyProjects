bae2758e455e4ca31dfca0d817bf791bb55c3f64
Danilo <methodic4u@gmail.com>
2020-04-02 20:46:33 -0300

    Still working on layout

--- border-radius-previwer/src/components/previwer/testSpace/TesteSpace.js
+++ border-radius-previwer/src/components/previwer/testSpace/TesteSpace.js
@@ -4,8 +4,8 @@ import classes from './TesteSpace.module.css';
 const testSpace = (props) => {
     return (
         <div className={classes.TestSpace} >
-            <div style={{border: 'blue 1px solid', borderRadius: "15%", margin: '2%', display: 'flex', 'align-items': 'stretch'}}>
-                test element
+            <div className={classes.TestElement} >
+                
             </div>
         </div>
     );
